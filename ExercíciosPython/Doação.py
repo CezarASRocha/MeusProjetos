cf74eef43c61dce64996815f100e39162b393308
Cezar Rocha <cezar.a.rocha@hotmail.com>
2022-10-31 17:19:53 -0300

    Exercício
    Entrada:
    Diversos números reais, um por linha, que representam os valores das doações feitas em Vic Coin. A entrada é finalizada quando o número -1.0 for lido e, evidentemente, não deverá ser contabilizado como doação.
    
    Saída:
    A saída será composta de duas linhas:
    
    A primeira linha será um número real com duas casas decimais indicando o total doado em Vic Coins (VC$);A segunda linha será um número real com duas casas decimais indicando o total doado em reais (R$).

--- ExercíciosPython/Doação.py
+++ ExercíciosPython/Doação.py
@@ -1,11 +1,11 @@
-VicCoin = float(input())
+VicCoin = float(0)
 soma_quantidade = 0
 
 while VicCoin !=-1:
     VicCoin = float(input())
-    soma_quantidade += 1
-    if VicCoin == -1:
-        soma_quantidade += 1
-        reais = VicCoin * 2.50
-        print(f'VC$ {VicCoin:.2f}')
+    soma_quantidade += VicCoin
+    if VicCoin ==-1:
+        soma_quantidade +=1
+        reais = soma_quantidade * 2.50
+        print(f'VC$ {soma_quantidade:.2f}')
         print(f'R$ {reais:.2f}')
